18be32adf190bcda43a0042689304e655221a367
yangliangbo <yangliangbo@pinpianyi.com>
2020-12-20 11:52:02 +0800

    gitalk配置

--- .vitepress/theme/components/Comment.vue
+++ .vitepress/theme/components/Comment.vue
@@ -1,22 +1,23 @@
 // Comment.vue
 <template>
-  <div id="gitalk-container"></div>
+	<div id="gitalk-container"></div>
 </template>
 
 <script>
-  export default {
-    name: "comment",
-    mounted() {
-      const commentConfig = {
-        clientID: "c6bccd90d050b8194d55",
-        clientSecret: "e501eb9f9882bc8bdea03ae11a5c538464393665",
-        repo: "https://github.com/webylb/webylb.github.io",
-        owner: "webylb",
-        admin: ["webylb"],
-        id: location.pathname
-      };
-      const gitalk = new Gitalk(commentConfig);
-      gitalk.render("gitalk-container");
-    },
-  };
+export default {
+	name: 'comment',
+	mounted() {
+		const commentConfig = {
+			clientID: 'c6bccd90d050b8194d55',
+			clientSecret: 'e501eb9f9882bc8bdea03ae11a5c538464393665',
+			repo: 'webylb.github.io',
+			owner: 'webylb',
+			admin: ['webylb'],
+			id: location.pathname.substr(0, 50),
+			distractionFreeMode: false,
+		}
+		const gitalk = new Gitalk(commentConfig)
+		gitalk.render('gitalk-container')
+	}
+}
 </script>
